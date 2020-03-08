3f7697c0a7ac1733d9436d0ccaced13fed1545b4
Draguta Dan <dragutadan@gmail.com>
2020-03-08 12:09:58 +0200

    Workable version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,114 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section class="hero is-info">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          For women day
+        </h1>
+        <h2 class="subtitle has-text-weight-semibold">
+          Would you like to introduce yourself? becauese I would like to wish
+          you a happy birthday
+        </h2>
+
+        <b-field label="Username" class="has-text-white has-text-center">
+          <b-input v-model="username" class="has-text-centered" placeholder="Nickname"></b-input>
+        </b-field>
+        <p
+          class="is-size-1 has-text-weight-semibold"
+          v-if="setUsername.length > 2"
+        >
+          Happy Birthday to you {{ setUsername }}!
+        </p>
+        <div v-if="setUsername.length > 2">
+          <h2 class="subtitle has-text-weight-semibold">
+            I also prepared a gif for you <span class="is-size-1 has-text-weight-semibold">
+            <i class="fas fa-gift has-text-warning"></i>
+          </span>
+          </h2>
+          
+
+          <div class="block has-text-warning">
+            <b-radio v-model="radio" name="name" native-value="flower">
+              <span class="is-size-1 has-text-weight-semibold">
+            <i class="fas fa-gift"></i>
+          </span>
+              <strong class="has-text-white">Gift 1</strong>
+            </b-radio>
+            <b-radio v-model="radio" name="name" native-value="cake">
+              <span class="is-size-1 has-text-weight-semibold">
+            <i class="fas fa-gift"></i>
+          </span>
+              <strong class="has-text-white">Gift 2</strong>
+            </b-radio>
+            <b-radio v-model="radio" name="name" native-value="ladybug">
+              <span class="is-size-1 has-text-weight-semibold">
+            <i class="fas fa-gift"></i>
+          </span>
+              <strong class="has-text-white">Gift 3</strong>
+            </b-radio>
+          </div>
+          <p class="content">
+            <b>You choose the</b>
+            <b> {{ setGift }} </b>
+          </p>
+
+          <div>
+            <span v-if="setGift === 'flower'">
+              <img
+                class="image-style"
+                src="@/assets/images/flower.jpg"
+                alt=""
+              />
+              <p class="is-size-1 has-text-weight-semibold">
+                May the sun light your path
+              </p>
+            </span>
+            <span v-else-if="setGift === 'cake'">
+              <img class="image-style" src="@/assets/images/cake.jpg" alt="" />
+              <p class="is-size-1 has-text-weight-semibold">
+                May this cake make your life sweater
+              </p>
+            </span>
+            <span v-else-if="setGift === 'ladybug'">
+              <img
+                class="image-style"
+                src="@/assets/images/ladybug.jpg"
+                alt=""
+              />
+              <p class="is-size-1 has-text-weight-semibold">
+                May the Ladybug bring you luck and fortune
+              </p>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+
   props: {
     msg: String
+  },
+  data() {
+    return {
+      username: "",
+      radio: ""
+    };
+  },
+  computed: {
+    setUsername() {
+      return this.username.toUpperCase();
+    },
+
+    setGift() {
+      return this.radio;
+    }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +126,10 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.image-style {
+  height: 400px;
+  width: 400px;
 }
 </style>
